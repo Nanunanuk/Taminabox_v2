1b5f2da3e5eed0a47aab6a0b27c3b23523513743
nils.reiners@posteo.de <nils.reiners@posteo.de>
2019-09-13 21:39:26 +0100

    modified version

--- MyRead.py
+++ MyRead.py
@@ -25,14 +25,15 @@ print("Welcome to the MFRC522 data read example")
 print("Press Ctrl-C to stop.")
 
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
+formerWord=""
 while continue_reading:
     
     # Scan for cards    
     (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # If a card is found
-    if status == MIFAREReader.MI_OK:
-        print("Card detected")
+    #if status == MIFAREReader.MI_OK:
+    #    print("Card detected")
     
     # Get the UID of the card
     (status,uid) = MIFAREReader.MFRC522_Anticoll()
@@ -41,7 +42,7 @@ while continue_reading:
     if status == MIFAREReader.MI_OK:
 
         # Print UID
-        print("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
+        #print("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
     
         # This is the default key for authentication
         key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
@@ -54,7 +55,12 @@ while continue_reading:
 
         # Check if authenticated
         if status == MIFAREReader.MI_OK:
-            MIFAREReader.MFRC522_Read(8)
+            data=MIFAREReader.MFRC522_Read(8)
+            text="".join(chr(x) for x in data)
+            #print(text)
+            if 'Buena Vista' in text and 'Buena Vista'!=formerWord:
+                print("YES!")
+                formerWord='Buena Vista'
             MIFAREReader.MFRC522_StopCrypto1()
         else:
             print("Authentication error")
